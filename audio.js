7eab63368a24915f5d8490aa58c4658a00e73116
SUPER-Slaya <mystreamingandvideos@gmail.com>
2026-05-08 23:50:32 -0400

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,1 +1,13 @@
+const Audio = {
+  ctx: new (AudioContext||webkitAudioContext)(),
 
+  shoot() {
+    let o = this.ctx.createOscillator();
+    let g = this.ctx.createGain();
+    o.connect(g); g.connect(this.ctx.destination);
+    o.frequency.value = 600;
+    g.gain.value = 0.05;
+    g.gain.exponentialRampToValueAtTime(0.0001,this.ctx.currentTime+0.05);
+    o.start(); o.stop(this.ctx.currentTime+0.05);
+  }
+};
