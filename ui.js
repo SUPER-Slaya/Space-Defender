60aafbe3a416b1d4bf9c68193c587a9d544a81fb
SUPER-Slaya <mystreamingandvideos@gmail.com>
2026-05-08 23:51:09 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,7 @@
-
+const UI = {
+  update() {
+    document.getElementById("score").innerText = Game.score;
+    document.getElementById("lives").innerText = Game.lives;
+    document.getElementById("wave").innerText = Game.wave;
+  }
+};
